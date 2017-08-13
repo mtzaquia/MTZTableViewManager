23a8eea012a9d087632afc561f5425cadfaf8cc2
Mauricio T Zaquia <mauriciotzaquia@gmail.com>
2017-08-13 11:48:09 +0200

    add example.gif.

--- Example/Pods/Target Support Files/MTZExpirationDatePicker/MTZExpirationDatePicker-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MTZExpirationDatePicker : NSObject
-@end
-@implementation PodsDummy_MTZExpirationDatePicker
-@end
--- Example/Pods/Target Support Files/MTZTableViewManager/MTZTableViewManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MTZTableViewManager : NSObject
-@end
-@implementation PodsDummy_MTZTableViewManager
-@end
